9ab3ea75ec59e0e960eedb07ebd5df974acb8c48
leentaylor <leentaylor@gmail.com>
2023-09-25 08:57:55 +1000

    creational patterns wip

---
+++ design-patterns/sean-bradley/src/patterns/builder/example/builder-concept.ts
@@ -0,0 +1,54 @@
+// Builder Concept Sample Code
+class Product {
+  parts: string[] = [];
+}
+
+// why do our methods return a reference to this?
+// so we can chain the methods together buildPartA().buildPartC()
+interface IBuilder {
+  buildPartA(): this;
+  buildPartB(): this;
+  buildPartC(): this;
+  getResult(): Product;
+}
+
+class Builder implements IBuilder {
+  // The Concrete Builder
+  product: Product;
+
+  constructor() {
+    this.product = new Product();
+  }
+
+  buildPartA() {
+    this.product.parts.push("a");
+    return this;
+  }
+
+  buildPartB() {
+    this.product.parts.push("b");
+    return this;
+  }
+
+  buildPartC() {
+    this.product.parts.push("c");
+    return this;
+  }
+
+  getResult() {
+    return this.product;
+  }
+}
+
+class Director {
+  // The Director, building a complex representation
+
+  static construct() {
+    "Constructs and returns the final product";
+    return new Builder().buildPartA().buildPartB().buildPartC().getResult();
+  }
+}
+
+// The Client
+const PRODUCT1 = Director.construct();
+console.log(PRODUCT1.parts);
--- design-patterns/sean-bradley/src/patterns/factory/little-chair.ts
+++ design-patterns/sean-bradley/src/patterns/factory/little-chair.ts
@@ -1,3 +1,8 @@
 import { Chair } from "./chair";
 
-export class LittleChair extends Chair {}
+export class LittleChair extends Chair {
+  constructor() {
+    console.log("I added a custom constructor just to log this message");
+    super();
+  }
+}
